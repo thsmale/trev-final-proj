e217b7246a50174d0dcf4dfe8919b71f3d8366e3
thsmale <tsmale@gmail.com>
2023-04-21 16:22:38 -0700

    add cards in grid format w/out useContext

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import { 
 	Box,
 	Button, 
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Grid,
 	Grommet, 
 	grommet,
 	Header, 
+	Heading,
 	Page, 
 	PageContent, 
 	PageHeader, 
+	Paragraph,
 	Text 
 } from 'grommet';
 import { deepMerge } from 'grommet/utils';
@@ -34,6 +41,22 @@ const AppBar = props => {
 		{...props}
 	/>
 };
+
+const CardTemplate = ({ title }) => {
+	return (
+		<Card>
+			<CardHeader pad='medium'>
+				<Heading level={2} margin='none'> {title} </Heading>
+			</CardHeader>
+			<CardBody pad='medium'>
+				<Paragraph>Hello world</Paragraph>
+			</CardBody>
+			<CardFooter pad='medium' background='background-contrast'>
+				Footer
+			</CardFooter>
+		</Card>
+	);
+}
 
 const App = () => {
   const [dark, setDark] = useState(false);
@@ -62,6 +85,10 @@ const App = () => {
 		      </AppBar>
 		      <PageContent>
 			      <PageHeader title="Trevor Final Project" />
+			      <Grid columns='medium' gap='large' pad={{ bottom: 'large' }}>
+				      <CardTemplate title={'Card 1'} />
+				      <CardTemplate title={'Card 2'} />
+			      </Grid>
 		      </PageContent>
 	      </Page>
       </Grommet>
