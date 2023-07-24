2088c95f887cc14800857251605e1eaf60f79c93
thsmale <tsmale@gmail.com>
2023-07-24 02:05:40 -0700

    integrate successfully with one bill
    UI requires there to be at least two people on the tab

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from venmo import receipt_to_df
 from algo import calc_pay
-import json
 
 app = FastAPI()
 
@@ -22,15 +21,6 @@ app.add_middleware(
 async def post_bill(request: Request):
     payload = await request.body()
     payload = payload.decode('utf-8')
-    """
-    print(payload)
-    pdict = json.loads(payload)
-    print(pdict)
-
-    Returns convert_format ptyhon_dict = json.loads(json_data) JSON object must be str, bytes or bytearray, not DataFrame
-    payload = pd.read_json(payload)
-    print(payload)
-    """
     dfs, master, df_name_date, df_list = receipt_to_df(payload)
     payments_df, instructions = calc_pay(master)
     print(payments_df)
